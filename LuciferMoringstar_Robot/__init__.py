5e4033aedbe0099d46dc27173143c32adaa0607f
AaGath <89765070+AaGath@users.noreply.github.com>
2021-10-02 11:39:27 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -15,29 +15,29 @@ Basic Commads
 
 /start :- check am Alive
 
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+PrimeFlix
+Since 2021
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+PrimeFlix
+PrimeFlix
+PrimeFlix
+PrimeFlix
+PrimeFlix
+PrimeFlix
 
-@Mo_Tech_YT
+[🖥️ More Movies 🖥️](https://t.me/PrimeFlixMovies)
+
+@PrimeFlix_Movies
 """
 
 ABOUT = """
 ➥ Name :  Auto Filter With IMDB
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [Cyber Dom](t.me/Cyber_Dom)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [Click Me](https://github.com/odysseusmax/animated-lamp)
 """
