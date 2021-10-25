a7b547e985711f0257a0229276dd7c3eaad033bf
AaGath <89765070+AaGath@users.noreply.github.com>
2021-10-25 17:48:31 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -58,9 +58,9 @@ async def filter(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = 🗽 f"[{get_size(file.file_size)}] 🗂 {file.file_name}"
+                filename = f"[{get_size(file.file_size)}] 🗂 {file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}🗂",callback_data=f"pr0fess0r_99#{file_id}")]
+                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
